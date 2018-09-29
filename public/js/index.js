b0bda3226f38f6452d3ab83fb4e21825c71f0346
ptirumala <ptirumala@zapoj.com>
2018-09-29 16:07:11 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 var socket = io();
 
-		socket.on('connect', function() {
-			console.log('connected to server');			
-		});
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
 
-		socket.on('disconnect', function() {
-			console.log('Disconnected from server');
-		});	
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
-		socket.on('newMessage', function(message) {
-			console.log('NewMessage', message);
-		});
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
